aa335e0ac7b704b0aa96731f79eebfff840fa4e0
Tirtharaj <tirtharajjana615@gmail.com>
2021-06-20 17:02:56 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
-
+import AddTransection from './components/AddTransection';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransectionList from './components/TransectionList';
 function App() {
   return (
     <div >
-      Hello world
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransectionList />
+        <AddTransection />
+      </div>
     </div>
   );
 }
 
-export default App;             
+export default App;
